d108bfb692d7be28d255a008511eb70f3e9335f2
Katherine <katherine.smith1234@gmail.com>
2019-09-27 23:46:46 +0100

    Add spec fixtures and specs for distance calculator service

--- spec/customer_locator_spec.rb
+++ spec/customer_locator_spec.rb
@@ -1,16 +1,46 @@
 require 'customer_locator'
 
 RSpec.describe CustomerLocator, "#list_customers" do
+  let(:locator) { CustomerLocator.new }
+
   context "given list of customers" do
-    it "returns those within 100km" do
-      locator = CustomerLocator.new
+    it "returns all customers when everyone within 100km" do
       correct_list = [{
+        latitude: "52.986375",
+        user_id: 12,
         name: "Christina McArdle",
-        id: 12
+        longitude: "-6.043701"
+      }, {
+        latitude: "53.008769",
+        user_id: 11,
+        name: "Richard Finnegan",
+        longitude: "-6.1056711"
       }]
 
-      # TODO: I don't think this kind of comparison actually works for arrays/arrays of objects
-      expect(locator.list_customers).to eq correct_list
+      expect(locator.list_customers("./spec/fixtures/files/all_customers_within_100km.txt")).to eq correct_list
+    end
+
+    it "returns nothing when no one is within 100km" do
+      empty_list = []
+
+      expect(locator.list_customers("./spec/fixtures/files/no_customers_within_100km.txt")).to eq empty_list
+    end
+
+    it "returns only customers within 100km" do
+      correct_list = [{
+        latitude: "54.133333",
+        user_id: 24,
+        name: "Rose Enright",
+        longitude: "-6.433333"
+      }]
+
+      expect(locator.list_customers("./spec/fixtures/files/some_customers_within_100km.txt")).to eq correct_list
+    end
+  end
+
+  context "file doesn't exist" do
+    it "throws error loading file" do
+      expect { locator.list_customers("./not/a/real/file.txt") }.to raise_error(Errno::ENOENT)
     end
   end
 end
---
+++ spec/distance_calculator_spec.rb
@@ -0,0 +1,45 @@
+class DummyClass
+  include DistanceCalculator
+end
+
+RSpec.describe DistanceCalculator, "#convert_to_radians" do
+  let(:subject) { DummyClass.new }
+
+  context "given valid arguments" do
+    it "returns degrees in radians" do
+      expect(subject.convert_to_radians(42)).to eq 0.733038
+    end
+  end
+
+  context "given invalid arguments" do
+    it "throws error when given string" do
+      expect { subject.convert_to_radians('test') }.to raise_error(NoMethodError)
+    end
+
+    it "throws error when given nil" do
+      expect { subject.convert_to_radians(nil) }.to raise_error(NoMethodError)
+    end
+  end
+end
+
+RSpec.describe DistanceCalculator, "#calculate_distance_between" do
+  let(:subject) { DummyClass.new }
+
+  context "given valid arguments" do
+    it "returns distance between dublin and another point" do
+      lat = 0.932987655533
+      long = -0.108749354
+      expect(subject.calculate_distance_between(long, lat)).to eq 13.109199
+    end
+  end
+
+  context "given invalid arguments" do
+    it "throws error when given string" do
+      expect { subject.calculate_distance_between('not a longitude', 'not a latitude') }.to raise_error(TypeError)
+    end
+
+    it "throws error when given nil" do
+      expect { subject.calculate_distance_between('not a longitude', 'not a latitude') }.to raise_error(TypeError)
+    end
+  end
+end
