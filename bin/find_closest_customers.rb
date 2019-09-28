9ecf7dddd58416930a5b7a7d1b752c8a637c3988
Katherine <katherine.smith1234@gmail.com>
2019-09-28 20:51:34 +0100

    Add script to find customers within 100km of dublin office and output file produced

---
+++ bin/find_closest_customers.rb
@@ -0,0 +1,37 @@
+require './lib/customer_locator'
+
+puts 'Finding customers...'
+
+file_path = CustomerLocator::DEFAULT_FILE_PATH
+distance = CustomerLocator::DEFAULT_DISTANCE_KM
+
+file_arg_index = ARGV.find_index("--filepath")
+distance_arg_index = ARGV.find_index("--distance")
+
+unless file_arg_index.nil?
+  file_path_value = file_arg_index + 1
+  file_path = ARGV[file_path_value].to_s
+end
+
+unless distance_arg_index.nil?
+  distance_value_index = distance_arg_index + 1
+  distance = ARGV[distance_value_index].to_f
+end
+
+locator = CustomerLocator.new(file_path: file_path, distance: distance)
+customers = locator.call
+
+puts "Found #{customers.count} to invite!"
+puts customers.join("\n")
+
+output_file_name = "output.txt"
+
+if File.exists?(output_file_name)
+  puts "Output file #{output_file_name} already exists, overwriting..."
+end
+
+File.open(output_file_name, "w") do |f|
+  f.write(customers)
+end
+
+puts "All done!"
