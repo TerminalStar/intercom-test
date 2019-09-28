26a2473d48b7caffc9115dcbe8fb44352251719a
Katherine <katherine.smith1234@gmail.com>
2019-09-28 20:50:51 +0100

    Add spec for json file reader, update fixture file names

--- spec/customer_locator_spec.rb
+++ spec/customer_locator_spec.rb
@@ -1,46 +1,64 @@
 require 'customer_locator'
 
-RSpec.describe CustomerLocator, "#list_customers" do
+RSpec.describe CustomerLocator, "#call" do
   let(:locator) { CustomerLocator.new }
+
+  def compare_response(test_data, correct_list)
+    allow(locator).to receive(:read_file).and_return(test_data)
+    expect(locator.call).to eq correct_list
+  end
 
   context "given list of customers" do
     it "returns all customers when everyone within 100km" do
+      test_data = "[{\"latitude\": \"52.986375\", \"user_id\": 12, \"name\": \"Christina McArdle\", \"longitude\": \"-6.043701\"},{\"latitude\": \"53.008769\", \"user_id\": 11, \"name\": \"Richard Finnegan\", \"longitude\": \"-6.1056711\"}]"
       correct_list = [{
-        latitude: "52.986375",
-        user_id: 12,
-        name: "Christina McArdle",
-        longitude: "-6.043701"
+        id: 11,
+        name: "Richard Finnegan"
       }, {
-        latitude: "53.008769",
-        user_id: 11,
-        name: "Richard Finnegan",
-        longitude: "-6.1056711"
+        id: 12,
+        name: "Christina McArdle"
       }]
 
-      expect(locator.list_customers("./spec/fixtures/files/all_customers_within_100km.txt")).to eq correct_list
+      compare_response(test_data, correct_list)
     end
 
     it "returns nothing when no one is within 100km" do
+      test_data = "[{\"latitude\": \"51.92893\", \"user_id\": 1, \"name\": \"Alice Cahill\", \"longitude\": \"-10.27699\"},{\"latitude\": \"52.833502\", \"user_id\": 25, \"name\": \"David Behan\", \"longitude\": \"-8.522366\"}]"
       empty_list = []
 
-      expect(locator.list_customers("./spec/fixtures/files/no_customers_within_100km.txt")).to eq empty_list
+      compare_response(test_data, empty_list)
     end
 
     it "returns only customers within 100km" do
+      test_data = "[{\"latitude\": \"54.133333\", \"user_id\": 24, \"name\": \"Rose Enright\", \"longitude\": \"-6.433333\"},{\"latitude\": \"55.033\", \"user_id\": 19, \"name\": \"Enid Cahill\", \"longitude\": \"-8.112\"},{\"latitude\": \"51.92893\", \"user_id\": 1, \"name\": \"Alice Cahill\", \"longitude\": \"-10.27699\"}]"
       correct_list = [{
-        latitude: "54.133333",
-        user_id: 24,
-        name: "Rose Enright",
-        longitude: "-6.433333"
+        id: 24,
+        name: "Rose Enright"
       }]
 
-      expect(locator.list_customers("./spec/fixtures/files/some_customers_within_100km.txt")).to eq correct_list
+      compare_response(test_data, correct_list)
     end
   end
 
-  context "file doesn't exist" do
-    it "throws error loading file" do
-      expect { locator.list_customers("./not/a/real/file.txt") }.to raise_error(Errno::ENOENT)
+  context "given invalid params" do
+    context "given invalid distance value" do
+      it "throws error when distance is string" do
+        locator = CustomerLocator.new(
+          distance: 'not really a distance'
+        )
+
+        allow(locator).to receive(:read_file).and_return("[{}]")
+        expect { locator.call }.to raise_error(ArgumentError)
+      end
+
+      it "throws error when distance is nil" do
+        locator = CustomerLocator.new(
+          distance: nil
+        )
+
+        allow(locator).to receive(:read_file).and_return("[{}]")
+        expect { locator.call }.to raise_error(ArgumentError)
+      end
     end
   end
 end
---
+++ spec/json_file_reader_spec.rb
@@ -0,0 +1,34 @@
+class DummyClass
+  include JsonFileReader
+end
+
+RSpec.describe JsonFileReader, "#read_file" do
+  let(:subject) { DummyClass.new }
+
+  context "given file of expected format" do
+    it "reads file and converts to valid JSON" do
+      file_contents = subject.read_file("./spec/fixtures/files/example_customers.txt")
+      expected_json = [
+        { "latitude" => "54.133333", "user_id" => 24, "name" => "Rose Enright", "longitude" => "-6.433333" },
+        { "latitude" => "55.033", "user_id" => 19, "name" => "Enid Cahill", "longitude" => "-8.112" },
+        { "latitude" => "51.92893", "user_id" => 1, "name" => "Alice Cahill", "longitude" => "-10.27699" }
+      ]
+
+      expect(JSON.parse(file_contents)).to eq expected_json
+    end
+  end
+
+  context "given malformed file" do
+    it "throws an error parsing the malformed JSON" do
+      file_contents = subject.read_file("./spec/fixtures/files/malformed_json.txt")
+
+      expect { JSON.parse(file_contents) }.to raise_error(JSON::ParserError)
+    end
+  end
+
+  context "given file that does not exist" do
+    it "throws error loading file" do
+      expect { subject.read_file("./not/a/real/file.txt") }.to raise_error(Errno::ENOENT)
+    end
+  end
+end
